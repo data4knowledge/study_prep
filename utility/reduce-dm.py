61337aa9794c2ee0bbe43446496815dfee7e2069
jozza <jozza@users.noreply.github.com>
2024-03-15 13:16:05 +0100

    Reduced dm

--- utility/reduce-dm.py
+++ utility/reduce-dm.py
@@ -9,22 +9,29 @@ def get_data(file):
     if len(list(data[0].keys())) < 3:
         print("I don't think the csv was read properly")
     return data
-    # with open(file, 'r') as file:
-    #     header = file.readline().strip().split("\t")
-    #     data = []
-    #     for line in file.readlines():
-    #         values = line.strip().split("\t")
-    #         x = dict(map(lambda i,j : (i,j) , header,values))
-    #         data.append(x)
-    # return data
 
-DM_DATA = Path.cwd() / "tmp" / "dm-full.csv"
-assert DM_DATA.exists(), "DM_DATA not found"
+FULL_DM_DATA = Path.cwd() / "tmp" / "dm-full.csv"
+assert FULL_DM_DATA.exists(), "FULL_DM_DATA not found"
 
-dm = get_data(DM_DATA)
+dm = get_data(FULL_DM_DATA)
 
-subjects = [x['USUBJID'] for x in dm][0:10]
-print("subjects",subjects)
+# subjects = [x['USUBJID'] for x in dm][0:10]
+dm_data = [x for x in dm][0:10]
+print("len(dm_data)",len(dm_data))
+
+DM_JSON = Path.cwd() / "tmp" / "dm.json"
+with open(DM_JSON, 'w') as f:
+    f.write(json.dumps(dm_data, indent = 2))
+
+DM_CSV = Path.cwd() / "data" / "input" / "dm.csv"
+print("Saving to",DM_CSV)
+output_variables=list(dm_data[0].keys())
+with open(DM_CSV, 'w') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=output_variables)
+    writer.writeheader()
+    writer.writerows(dm_data)
+print("Done")
+
 
 exit()
 
@@ -35,9 +42,6 @@ print("len(vs)",len(vs))
 vs_data = [x for x in vs if x['USUBJID'] in subjects and x['VSTEST'] in LABELS]
 print("len(vs_data)",len(vs_data))
 
-VS_DATA = Path.cwd() / "data" / "vs.json"
-with open(VS_DATA, 'w') as f:
-    f.write(json.dumps(vs_data, indent = 2))
 
 VS_DATA = Path.cwd() / "data" / "vs.tsv"
 with open(VS_DATA, 'w') as file:
