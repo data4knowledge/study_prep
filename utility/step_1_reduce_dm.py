6ced1fa732290c935175d8cc70a11b162909a75e
Johannes Ulander <jozza@users.noreply.github.com>
2024-06-27 11:58:30 +0200

    Changed from script to method that can be imported

--- utility/step_1_reduce_dm.py
+++ utility/step_1_reduce_dm.py
@@ -12,18 +12,23 @@ def get_xpt_data(file):
     return data
 
 
-FULL_DM_DATA = Path.cwd() / "tmp" / "dm.xpt"
-print("Reading",FULL_DM_DATA)
-assert FULL_DM_DATA.exists(), "FULL_DM_DATA not found"
+def reduce_dm():
+    FULL_DM_DATA = Path.cwd() / "tmp" / "dm.xpt"
+    print("Reading",FULL_DM_DATA)
+    assert FULL_DM_DATA.exists(), "FULL_DM_DATA not found"
 
-dm = get_xpt_data(FULL_DM_DATA)
-print("full len(dm)",len(dm))
+    dm = get_xpt_data(FULL_DM_DATA)
+    print("full len(dm)",len(dm))
 
-dm_data = [x for x in dm[0:10]]
-print("reduced len(dm_data)",len(dm_data))
+    dm_data = [x for x in dm[0:10]]
+    print("reduced len(dm_data)",len(dm_data))
 
-DM_JSON = Path.cwd() / "data" / "input" / "dm.json"
-print("Saving",DM_JSON)
-with open(DM_JSON, 'w') as f:
-    f.write(json.dumps(dm_data, indent = 2))
-print("done")
+    DM_JSON = Path.cwd() / "data" / "input" / "dm.json"
+    print("Saving",DM_JSON)
+    with open(DM_JSON, 'w') as f:
+        f.write(json.dumps(dm_data, indent = 2))
+    print("done")
+
+if __name__ == "__main__":
+    print("not running as main")
+    # reduce_dm()
