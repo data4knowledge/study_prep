001fc3d402353b1d72c2805e0d6026507bbbb9c3
jozza <jozza@users.noreply.github.com>
2024-07-04 12:43:26 +0200

    Made BRTHDTC a little more trustworthy

--- utility/pre/step_1_reduce_dm.py
+++ utility/pre/step_1_reduce_dm.py
@@ -18,13 +18,31 @@ def reduce_dm():
     dm = get_xpt_data(FULL_DM_DATA)
     print("full len(dm)",len(dm))
 
+    birth_dates = [
+        '1980-06-30',
+        '1978-03-01',
+        '1985-03-01',
+        '1970-05-24',
+        '1978-07-13',
+        '1983-06-13',
+        '1982-10-13',
+        '1982-10-13',
+        '1967-12-10',
+        '1972-11-22',
+    ]
+
     # dm_data = [x for x in dm[0:10]]
     dm_data = []
     print("N.B! Faking 1) RFICDTC from DMDTC 2) BRTHDTC is hardcoded")
+    i = 0
     for row in dm[0:10]:
         row['RFICDTC'] = row['DMDTC']
-        row['BRTHDTC'] = '1980-01-01'
+        if i < len(birth_dates):
+            row['BRTHDTC'] = birth_dates[i]
+        else:
+            row['BRTHDTC'] = '1980-01-01'
         dm_data.append(row)
+        i += 1
 
     print("reduced len(dm_data)",len(dm_data))
 
