4e8510454bd49810f6fb425b97ac567e3489f015
jozza <jozza@users.noreply.github.com>
2024-07-05 11:06:06 +0200

    Fix db query handling

--- step_0_create_data_contract_lookup.py
+++ step_0_create_data_contract_lookup.py
@@ -43,6 +43,43 @@ def clean(txt: str):
 def add_issue(*txts):
     issues.append(" ".join(txts))
 
+def db_query(query):
+    db = Neo4jConnection()
+    result = []
+    with db.session() as session:
+        response = session.run(query)
+        if response == None:
+            print('query did not work"',query)
+            exit()
+        result = [x.data() for x in response]
+    db.close()
+    return result
+
+
+def list_dc_bcp_name():
+    OUTPUT_FILE = OUTPUT_PATH / "data_contracts.json"
+    with open(OUTPUT_FILE) as f:
+        data_contracts = json.load(f)
+
+    results = []
+    add_debug("=== Check DC_URIs")
+    for dc in data_contracts:
+        # add_debug(dc['DC_URI'])
+        query = f"""
+            match (dc:DataContract)-[:PROPERTIES_REL]->(bcp:BiomedicalConceptProperty)<-[:PROPERTIES_REL]-(bc:BiomedicalConcept)
+            WHERE dc.uri = '{dc['DC_URI']}'
+            return bcp.name as bcp_name, bc.name as bc_name
+        """
+        bcp_names = db_query(query)            
+        if bcp_names:
+            bcp_name = bcp_names[0]['bcp_name']
+            bc_name = bcp_names[0]['bc_name']
+            results.append(f"{bc_name} {bcp_name} : {dc['DC_URI']}")
+        else:
+            results.append(f"MISS {dc['DC_URI']}")
+            print("!!! Data Conctract not found:", dc['DC_URI'])
+    return results
+
 def check_that_data_contracts_exist():
     OUTPUT_FILE = OUTPUT_PATH / "data_contracts.json"
     with open(OUTPUT_FILE) as f:
@@ -50,87 +87,80 @@ def check_that_data_contracts_exist():
 
     missing_dcs = 0
     add_debug("=== Check DC_URIs")
-    db = Neo4jConnection()
-    with db.session() as session:
-        for dc in data_contracts:
-            # add_debug(dc['DC_URI'])
-            query = f"""
-                match (dc:DataContract) WHERE dc.uri = '{dc['DC_URI']}'
-                return dc.uri as uri
-            """
-            results = session.run(query)
-            if results == None:
-                add_debug("DC not found:",dc['DC_URI'])
-                missing_dcs += 1
-            #     add_issue("timeline DataContract query has errors in it",visit,bc_label,tpt,query)
-            #     return []
-            else:
-                pass
-                # 
-                # add_debug("found:",results)
+    for dc in data_contracts:
+        # add_debug(dc['DC_URI'])
+        query = f"""
+            match (dc:DataContract) WHERE dc.uri = '{dc['DC_URI']}'
+            return dc.uri as uri
+        """
+        results = db_query(query)
+        if results == []:
+            add_debug("DC not found:",dc['DC_URI'])
+            missing_dcs += 1
+        else:
+            pass
     return missing_dcs
 
 
-def get_bc_properties(db, bc_label, row):
+def get_bc_properties( bc_label, row):
+    results = []
     if row['VISIT'] in DATA_VISITS_TO_ENCOUNTER_LABELS:
         visit = DATA_VISITS_TO_ENCOUNTER_LABELS[row['VISIT']]
     else:
         add_issue("visit not found:",row['VISIT'])
         return []
-    with db.session() as session:
-        query = f"""
+    query = f"""
         MATCH (bc:BiomedicalConcept)-[:PROPERTIES_REL]->(bcp)<-[:PROPERTIES_REL]-(dc:DataContract)-[:INSTANCES_REL]->(act_inst)-[:ENCOUNTER_REL]-(enc)
         WHERE enc.label = '{visit}'
         AND  bc.label = '{bc_label}'
         return bc.label as BC_LABEL, bcp.name as BCP_NAME, bcp.label as BCP_LABEL, enc.label as ENCOUNTER_LABEL, dc.uri as DC_URI
-        """
-        results = session.run(query)
-        if results == None:
-            add_issue("DataContract has errors in it",row['VISIT'],visit,bc_label,query)
-            return []
-        return [result.data() for result in results]
+    """
+    results = db_query(query)
+    if results == []:
+        add_issue("DataContract has errors in it",row['VISIT'],visit,bc_label,query)
+        return []
+    return results
 
-def get_bc_properties_dm(db, bc_label, dm_visit):
+def get_bc_properties_dm( bc_label, dm_visit):
     if dm_visit in DATA_VISITS_TO_ENCOUNTER_LABELS:
         visit = DATA_VISITS_TO_ENCOUNTER_LABELS[dm_visit]
     else:
         add_issue("visit not found:",dm_visit)
         return []
-    with db.session() as session:
-        query = f"""
+    query = f"""
         MATCH (bc:BiomedicalConcept)-[:PROPERTIES_REL]->(bcp)<-[:PROPERTIES_REL]-(dc:DataContract)-[:INSTANCES_REL]->(act_inst)-[:ENCOUNTER_REL]-(enc)
         WHERE enc.label = '{visit}'
         AND  bc.label = '{bc_label}'
         return bc.label as BC_LABEL, bcp.name as BCP_NAME, bcp.label as BCP_LABEL, enc.label as ENCOUNTER_LABEL, dc.uri as DC_URI
-        """
-        results = session.run(query)
-        if results == None:
-            add_issue("DataContract has errors in it",visit,bc_label,query)
-            return []
-        return [result.data() for result in results]
+    """
+    results = db_query(query)
+    if results == []:
+        add_issue("DataContract has errors in it",visit,bc_label,query)
+        return []
+    return results
 
-def get_bc_properties_sub_timeline(db, bc_label, tpt, row):
+def get_bc_properties_sub_timeline(bc_label, tpt, row):
     if row['VISIT'] in DATA_VISITS_TO_ENCOUNTER_LABELS:
         visit = DATA_VISITS_TO_ENCOUNTER_LABELS[row['VISIT']]
     else:
         add_issue("visit not found:",row['VISIT'])
         return []
-    with db.session() as session:
-        query = f"""
-            match (msai:ScheduledActivityInstance)<-[:INSTANCES_REL]-(dc:DataContract)-[:INSTANCES_REL]-(ssai:ScheduledActivityInstance)
-            match (msai)-[:ENCOUNTER_REL]->(enc:Encounter)
-            match (ssai)<-[:RELATIVE_FROM_SCHEDULED_INSTANCE_REL]-(t:Timing)
-            match (dc)-[:PROPERTIES_REL]->(bcp:BiomedicalConceptProperty)<-[:PROPERTIES_REL]-(bc:BiomedicalConcept)
-            WHERE enc.label = '{visit}'
-            and    t.value = '{tpt}'
-            AND  bc.label = '{bc_label}'
-            return bc.label as BC_LABEL, bcp.name as BCP_NAME, bcp.label as BCP_LABEL, enc.label as ENCOUNTER_LABEL, t.value as TIMEPOINT_VALUE, dc.uri as DC_URI
-        """
-        results = session.run(query)
-        if results == None:
-            add_issue("timeline DataContract query has errors in it",visit,bc_label,tpt,query)
-            return []
-        return [result.data() for result in results]
+    query = f"""
+        match (msai:ScheduledActivityInstance)<-[:INSTANCES_REL]-(dc:DataContract)-[:INSTANCES_REL]-(ssai:ScheduledActivityInstance)
+        match (msai)-[:ENCOUNTER_REL]->(enc:Encounter)
+        match (ssai)<-[:RELATIVE_FROM_SCHEDULED_INSTANCE_REL]-(t:Timing)
+        match (dc)-[:PROPERTIES_REL]->(bcp:BiomedicalConceptProperty)<-[:PROPERTIES_REL]-(bc:BiomedicalConcept)
+        WHERE enc.label = '{visit}'
+        and    t.value = '{tpt}'
+        AND  bc.label = '{bc_label}'
+        return bc.label as BC_LABEL, bcp.name as BCP_NAME, bcp.label as BCP_LABEL, enc.label as ENCOUNTER_LABEL, t.value as TIMEPOINT_VALUE, dc.uri as DC_URI
+    """
+    results = db_query(query)
+    if results == []:
+        add_issue("timeline DataContract query has errors in it",visit,bc_label,tpt,query)
+        return []
+    else:
+        return results
 
 def create_data_contracts_lookup():
     global unique_data_contracts
@@ -185,10 +215,6 @@ def create_data_contracts_lookup():
         exit()
     print("connected")
 
-    db = Neo4jConnection()
-
-    # print("Looping")
-
     for row in unique_labels_visits:
         tpt = ""
         if row['TEST'] in DATA_LABELS_TO_BC_LABELS:
@@ -198,9 +224,9 @@ def create_data_contracts_lookup():
             add_issue("Add test",row['TEST'])
         if 'TPT' in row and row['TPT'] != "":
             tpt = DATA_TPT_TO_TIMING_LABELS[row['TPT']]
-            properties = get_bc_properties_sub_timeline(db, bc_label, tpt,row)
+            properties = get_bc_properties_sub_timeline(bc_label, tpt,row)
         else:
-            properties = get_bc_properties(db, bc_label,row)
+            properties = get_bc_properties(bc_label,row)
         if properties:
             matches.append([bc_label,row['VISIT'],tpt,[x['BCP_NAME'] for x in properties]])
         else:
@@ -214,7 +240,7 @@ def create_data_contracts_lookup():
     # Add DM stuff
     dm_visit ='SCREENING 1'
     bc_label = "Sex"
-    properties = get_bc_properties_dm(db,bc_label,dm_visit)
+    properties = get_bc_properties_dm(bc_label,dm_visit)
     if properties:
         matches.append([bc_label,[x['BCP_NAME'] for x in properties]])
     else:
@@ -225,7 +251,7 @@ def create_data_contracts_lookup():
         else:
             unique_data_contracts.append(property)
     bc_label = "Race"
-    properties = get_bc_properties_dm(db,bc_label,dm_visit)
+    properties = get_bc_properties_dm(bc_label,dm_visit)
     if properties:
         matches.append([bc_label,[x['BCP_NAME'] for x in properties]])
     else:
@@ -236,7 +262,7 @@ def create_data_contracts_lookup():
         else:
             unique_data_contracts.append(property)
     bc_label = "Informed Consent Obtained"
-    properties = get_bc_properties_dm(db,bc_label,dm_visit)
+    properties = get_bc_properties_dm(bc_label,dm_visit)
     if properties:
         matches.append([bc_label,[x['BCP_NAME'] for x in properties]])
     else:
@@ -247,7 +273,7 @@ def create_data_contracts_lookup():
         else:
             unique_data_contracts.append(property)
     bc_label = "Date of Birth"
-    properties = get_bc_properties_dm(db,bc_label,dm_visit)
+    properties = get_bc_properties_dm(bc_label,dm_visit)
     if properties:
         matches.append([bc_label,[x['BCP_NAME'] for x in properties]])
     else:
@@ -260,41 +286,40 @@ def create_data_contracts_lookup():
             add_debug("DoB adding",property)
             unique_data_contracts.append(property)
 
-
-    db.close()
-
     print("Number of contracts and properties:",len(unique_data_contracts))
     print("Number of matches with data:",len(matches))
     print("Number of mismatches       :",len(mismatches), "(e.g. visit not defined in study)")
     print("")
 
-    # # for issue in set([issues for issues in issues]):
-    # #     print(issue,end="")
-    # # print("")
-    # # write_debug("debug-dc-issues.txt",issues)
-    # # write_debug("debug-dc.txt",mismatches)
-    # # write_debug("debug-dc-matches.txt",matches)
-    # add_debug("== Issues")
-    # [add_debug(x) for x in issues]
-    # add_debug("\n== mismaches")
-    # [add_debug(x) for x in mismatches]
-    # add_debug("\n== matches")
-    # [add_debug(x) for x in matches]
+    for issue in set([issues for issues in issues]):
+        print(issue)
+    add_debug("== Issues")
+    [add_debug(x) for x in issues]
+    add_debug("\n== mismaches")
+    [add_debug(x) for x in mismatches]
+    add_debug("\n== matches")
+    [add_debug(x) for x in matches]
 
     # Sort before saving, so it is easier to spot differences
     unique_data_contracts = sorted(unique_data_contracts, key=itemgetter('BC_LABEL','BCP_NAME','BCP_LABEL','ENCOUNTER_LABEL'))
 
     OUTPUT_FILE = OUTPUT_PATH / "data_contracts.json"
-    print("Saving to",OUTPUT_FILE, end="")
+    print("Saving to ",OUTPUT_FILE, end="")
     with open(OUTPUT_FILE, 'w') as f:
         f.write(json.dumps(unique_data_contracts, indent = 2))
-    print("done")
+    print(" done")
 
     count = check_that_data_contracts_exist()
     if count:
         print("\n-- THERE ARE MISSING DATA_CONTRACTS\n")
     else:
-        print("\n-- All data contracts exist\n")
+        print("\n-- All data contracts exist",count)
+    
+    list_of_dc_bcp = list_dc_bcp_name()
+    add_debug("\n== check DC and BCP name")
+    [add_debug(x) for x in list_of_dc_bcp]
+
+
     write_debug("step-0-debug-data-contracts-lookup.txt",debug)
 
 
