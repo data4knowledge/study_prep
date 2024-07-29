76ace052244e829368104f4bf690214141fb5295
Johannes Ulander <jozza@users.noreply.github.com>
2024-07-29 16:36:33 +0200

    Remove reduce data from run.py

--- run.py
+++ run.py
@@ -11,30 +11,21 @@ from utility.compare.compare_aura_local_bc import compare_bc
 from utility.compare.query_local import query_study_service
 from utility.pre.pre_step_check_mappings_against_db import check_mappings_against_db
 
-query_study_service()
-exit()
+# query_study_service()
+# exit()
 
 # print("\n== Compare db ")
 # compare_sdtm()
 # compare_bc()
 
-# # Pre-steps
-# from utility.pre.step_1_reduce_dm import reduce_dm
-# from utility.step_2_reduce_lb import reduce_lb
-# from utility.step_2_reduce_vs import reduce_vs
-# reduce_dm()
-# reduce_lb()
-# reduce_vs()
-
-
 print("\n== Check mappings against db")
 check_mappings_against_db()
 
 # exit()
-from utility.pre.pre_step_convert_surrogate_to_bc import convert_surrogate_to_bc
-from utility.pre.pre_step_link_informed_consent_to_dm import make_links
-convert_surrogate_to_bc()
-make_links()
+# from utility.pre.pre_step_convert_surrogate_to_bc import convert_surrogate_to_bc
+# from utility.pre.pre_step_link_informed_consent_to_dm import make_links
+# convert_surrogate_to_bc()
+# make_links()
 
 # Steps
 from step_0_create_data_contract_lookup import create_data_contracts_lookup
@@ -48,12 +39,12 @@ create_subject_enrolment_load_file()
 print("\n== Create data datapoints")
 create_subject_data_load_file()
 
-# Post steps
-from utility.post.post_step_load_datapoints import load_datapoints
-# print("\033[H\033[J") # Clears terminal window in vs code
+# # Post steps
+# from utility.post.post_step_load_datapoints import load_datapoints
+# # print("\033[H\033[J") # Clears terminal window in vs code
 
-print("\n== Load datapoint")
-load_datapoints()
+# print("\n== Load datapoint")
+# load_datapoints()
 
 # compare_crm()
 # compare_sdtm()
