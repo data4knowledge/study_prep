d784049f425e429089f4018b4b2a896eec3bba9c
jozza <jozza@users.noreply.github.com>
2024-08-13 23:22:13 +0200

    Added configuration

--- model/configuration.py
+++ model/configuration.py
@@ -2,16 +2,78 @@ import os
 import yaml
 from d4kms_service import Node, Neo4jConnection
 from d4kms_generic import ServiceEnvironment
+from uuid import uuid4
 from base_node import BaseNode
 from typing import List, Literal
 
 class ConfigurationNode(BaseNode):
   name: str = ""
-  type: str = ""
+  study_products_bcs: List[str]= []
+  disposition: List[str]= []
+  demography: List[str]= []
 
+  @classmethod
+  def create(cls, params):
+    db = Neo4jConnection()
+    with db.session() as session:
+      result = session.execute_write(cls._delete_configuration, cls)
+      result = session.execute_write(cls._create, cls, params)
+    db.close()
+    return result
+
+  @classmethod
+  def get(cls):
+    db = Neo4jConnection()
+    with db.session() as session:
+      result = session.execute_read(cls._get, cls)
+    db.close()
+    return result
+
+  @staticmethod
+  def _create(tx, cls, source_params):
+    params = []
+    for param in source_params:
+      if isinstance(source_params[param], list):
+        params.append(f"a.{param}={source_params[param]}")
+      else:
+        params.append(f"a.{param}='{source_params[param]}'")
+    params_str = ", ".join(params)
+    query = """
+      CREATE (a:%s {uuid: $uuid1})
+      SET %s 
+      RETURN a
+    """ % (cls.__name__, params_str)
+    # print("query",query)
+    result = tx.run(query, uuid1=str(uuid4()))
+    for row in result:
+      return cls.wrap(row['a'])
+    return None
+
+  @staticmethod
+  def _get(tx, cls):
+    query = """
+      MATCH (a:%s) RETURN a
+    """ % (cls.__name__)
+    print("query",query)
+    # result = tx.run(query, uuid1=str(uuid4()))
+    result = tx.run(query)
+    for row in result:
+      return cls.wrap(row['a'])
+    return None
+
+  @staticmethod
+  def _delete_configuration(tx, cls):
+    query = """
+      MATCH (a:%s) detach delete a
+      RETURN count(a)
+    """ % (cls.__name__)
+    print("query",query)
+    result = tx.run(query, uuid1=str(uuid4()))
+    for row in result:
+      return print("delete row",row)
+    return None
 
 class Configuration():
-  uuid: str
   DIR = 'data/input'
   FILENAME = 'configuration.yaml'
 
@@ -25,13 +87,12 @@ class Configuration():
       data = yaml.load(f, Loader=yaml.FullLoader)
     return data
 
+
 def main():
   c = Configuration()
-  ConfigurationNode.create(c._configuration)
-  for k,v in c._configuration.items():
-    print("k",k)
-    print("v",v)
   # print(c._configuration)
+  ConfigurationNode.create(c._configuration)
+  # configuration = ConfigurationNode.get()
 
 if __name__ == '__main__':
   main()
