50517c964f2602ee81e2dac721e0eba207cc17da
Johannes Ulander <jozza@users.noreply.github.com>
2024-09-26 17:15:22 +0200

    Removed old comments

--- utility/pre/step_11_reduce_dm_msg.py
+++ utility/pre/step_11_reduce_dm_msg.py
@@ -20,32 +20,10 @@ def reduce_dm():
     dm = get_xpt_data(FULL_DM_DATA)
     print("full len(dm)",len(dm))
 
-    birth_dates = [
-        '1980-06-30',
-        '1978-03-01',
-        '1985-03-01',
-        '1970-05-24',
-        '1978-07-13',
-        '1983-06-13',
-        '1982-10-13',
-        '1982-10-13',
-        '1967-12-10',
-        '1972-11-22',
-    ]
-
     # ['STUDYID', 'DOMAIN', 'USUBJID', 'SUBJID', 'RFSTDTC', 'RFENDTC', 'RFXSTDTC', 'RFXENDTC', 'RFICDTC', 'RFPENDTC', 'DTHDTC', 'DTHFL', 'SITEID', 'BRTHDTC', 'AGE', 'AGEU', 'SEX', 'RACE', 'ETHNIC', 'ARMCD', 'ARM', 'ACTARMCD', 'ACTARM', 'ARMNRS', 'ACTARMUD', 'COUNTRY']
     dm_data = []
-    # print("N.B! Faking 1) RFICDTC from DMDTC 2) BRTHDTC is hardcoded")
-    print("N.B! BRTHDTC is hardcoded")
-    i = 0
     for row in dm[0:10]:
-        # row['RFICDTC'] = row['DMDTC']
-        # if i < len(birth_dates):
-        #     row['BRTHDTC'] = birth_dates[i]
-        # else:
-        #     row['BRTHDTC'] = '1980-01-01'
         dm_data.append(row)
-        i += 1
 
     print("reduced len(dm_data)",len(dm_data))
 
