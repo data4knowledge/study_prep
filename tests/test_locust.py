aa6c0796462ce9d0fa35e4407b84f840f6273c59
Johannes Ulander <jozza@users.noreply.github.com>
2024-11-05 13:23:06 +0100

    Initial test

---
+++ tests/test_locust.py
@@ -0,0 +1,31 @@
+import time
+from locust import HttpUser, task, between
+
+class my_user(HttpUser):
+    @task
+    def root(self):
+        print("get root")
+        self.client.get("/")
+
+    @task
+    def studies(self):
+        print("get /studies")
+        self.client.get("/studies/")
+
+    @task
+    def cdisc_pilot(self):
+        print("get /cdisc_pilot")
+        self.client.get("/cdisc_pilot/")
+
+    @task
+    def forms(self):
+        print("get /cdisc_pilot")
+        self.client.get("/studyDesigns/096047c3-8954-4a96-a075-3a750b929ea8/forms")
+
+# def hello_world(self):
+#     print("get root")
+#     self.client.get("/")
+
+
+# class user(User):
+#     tasks = [hello_world]
