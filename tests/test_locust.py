e125dfaa25033031acd3e84d15e2501af0ebe17a
jozza <jozza@users.noreply.github.com>
2024-11-06 08:57:49 +0100

    Update test_locust.py

--- tests/test_locust.py
+++ tests/test_locust.py
@@ -4,20 +4,20 @@ from locust import HttpUser, task, between
 class my_user(HttpUser):
     @task
     def root(self):
-        print("get root")
+        # print("get root")
         self.client.get("/")
 
     @task
     def studies(self):
-        print("get /studies")
+        # print("get /studies")
         self.client.get("/studies/")
 
     @task
     def cdisc_pilot(self):
-        print("get /cdisc_pilot")
+        # print("get /cdisc_pilot")
         self.client.get("/cdisc_pilot/")
 
     @task
     def forms(self):
-        print("get /cdisc_pilot")
+        print("get /.../forms")
         self.client.get("/studyDesigns/096047c3-8954-4a96-a075-3a750b929ea8/forms")
